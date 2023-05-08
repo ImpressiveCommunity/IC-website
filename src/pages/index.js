14bdbdf369b869aed3a1ef17ecc1d980f9d97335
Sokmontrey <sythatsokmontrey@gmail.com>
2023-05-08 18:05:52 -0400

    make navbar responsive.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,9 @@
 import Layout from '../components/layout.js';
 
 export default function Home(){
-    return (<html>
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        </head>
-        <body>
-            <Layout current_page='home'>
-                <h1>Home</h1>
-            </Layout>
-        </body>
-    </html>);
+    return (<>
+        <Layout current_page='home'>
+            <h1>Home</h1>
+        </Layout>
+    </>);
 }
--- src/pages/programs_projects/index.js
+++ src/pages/programs_projects/index.js
@@ -7,7 +7,7 @@ import Events from './Events.jsx';
 export default function ProgramAndProjectPage(){
 
     return ( <Layout current_page='programs_projects'>
-        <div className='center items-center p-20
+        <div className='center items-center p-5 sm:p-10 md:p-20
             flex flex-col space-y-[8rem]'>
             <Overview />
             <Programs />
