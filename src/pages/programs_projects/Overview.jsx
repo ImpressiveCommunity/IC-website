ba03892da4cb1c437824126110a0c0ee250f2ca7
Sokmontrey <sythatsokmontrey@gmail.com>
2023-05-11 09:24:25 -0400

    content management system for program_project page

--- src/pages/programs_projects/Overview.jsx
+++ src/pages/programs_projects/Overview.jsx
@@ -1,14 +1,14 @@
+import {overview_detail } from './contents.js';
 import { HeadingFont, TextFont } from '../../styles/font.js';
 
 export default function Overview(){
     return (
         <div className='flex flex-col md:flex-row justify-between w-full'>
             <h1 className={'self-start md:sticky md:top-16 pb-5 ' + HeadingFont.className + ' text-4xl md:text-5xl'}>
-                Overview 
+                Overview
             </h1>
             <p className={TextFont.className + ' text-lg text-gray-700 w-full md:w-[50vw]'}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua<br/><br/>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                {overview_detail}
             </p>
         </div>
     )
