3ed9471653aae24e7c008163085a556208daf4ea
Sokmontrey <sythatsokmontrey@gmail.com>
2023-05-19 11:35:16 -0400

    styling home page and add mission section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,15 @@
-import { HeadingFont, TextFont } from '@/styles/font.js';
 import Layout from '../components/layout.js';
-import { ListOfPages } from '../components/elements/navbar.js';
+import Headline from './Headline.jsx';
+import OurMission from './OurMission.jsx';
 
 export default function Home(){
     return (<>
         <Layout current_page='home'>
-            <div className='w-full h-[100vh] flex flex-col items-center justify-center'>
-                <div className='flex space-x-20'>
-                    <div>
-                        <div className={HeadingFont.className + " text-8xl"}>
-                            <div className='absolute gradient-bg-1 w-[40vw] h-[30vh] z-[-1] opacity-[0.3]' />
-                            <h1 className='text-[#2dbdc0]'>Impressive</h1>
-                            <h1 className='text-[#fcba2a]'>Community</h1>
-                        </div>
-                        <div>
-                            <p className={TextFont.className + " text-gray-700 text-lg"}>Together for better KPC</p>
-                        </div>
-                    </div>
-                    <div className='flex flex-col items-left justify-center'>
-                        <ul className={'flex flex-col space-y-10 text-4xl'}>
-                            <ListOfPages />
-                        </ul>
-                    </div>
-                </div>
+            <Headline />
+            <div className='w-[100vw] flex justify-center'>
+                <div className='w-[80%] h-[1.2px] bg-gray-200' />
             </div>
+            <OurMission />
         </Layout>
     </>);
 }
