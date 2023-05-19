c65218eddb97bfd885977a1316feebaf7ac61d83
Sokmontrey <sythatsokmontrey@gmail.com>
2023-05-18 22:24:31 -0400

    basic home page

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,7 @@ export default function Layout({ children, current_page }){
             <script src="https://kit.fontawesome.com/814092e3ff.js" crossorigin="anonymous"></script>
         </Head>
         <Navbar current_page={current_page}/>
-        <main className='mt-16'>
+        <main className={current_page !== "home" ? 'mt-16' : ''}>
             {children}
         </main>
         <ContactInfo />
