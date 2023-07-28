7d1f6479c30139482b65bd88b26a5172002db761
Sodana <114868249+ssdn7@users.noreply.github.com>
2023-07-28 14:47:40 +0700

    Update contents.js

--- src/pages/programs_projects/contents.js
+++ src/pages/programs_projects/contents.js
@@ -27,11 +27,11 @@ export const programs = [
 
 export const events = [
     {
-        title: 'Event 1',
-        topic: 'Topic in Event 1',
+        title: 'Job Readiness Training for University Students',
+        topic: 'Conduct a series of 6 employability skill development sessions: The training will be designedto equip 15-20 university students with the necessary competencies for formal sector employment. By the end of this training, participants will enhance thier job readiness and stand out as attractive candidates to potential employers.',
         date: 'December 20, 2023',
         images_url: [
-            'https://images.pexels.com/photos/16401186/pexels-photo-16401186.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+            'https://staticlearn.shine.com/l/m/images/blog/career-growth-in-india.jpg',
         ]
     },
     {
