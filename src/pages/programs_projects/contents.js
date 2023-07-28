8747c59e9e3daf411f9328c30fbd25fd60f54b22
Sodana <114868249+ssdn7@users.noreply.github.com>
2023-07-28 15:08:18 +0700

    Update contents.js

--- src/pages/programs_projects/contents.js
+++ src/pages/programs_projects/contents.js
@@ -15,8 +15,10 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 
 export const programs = [
     {
-        title: 'Program 1',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+        title: 'Hi-Goal Camp',
+        description: 'This program aims to prepare students for the rigors of higher education by providing them with the academic skills, knowledge, and mindset they need to succeed. The program will also help students develop their critical thinking, problem-solving, and communication skills.
+And then this program also aims to help beneficiaries develop the entrepreneurial mindset and skills they need to succeed in the Cambodia startup ecosystem and digital economy. The program will cover topics such as business planning, marketing, fundraising, and product development.'
+    
     },
 
     {
@@ -29,12 +31,51 @@ export const events = [
     {
         title: 'Job Readiness Training for University Students',
         topic: 'Conduct a series of 6 employability skill development sessions: The training will be designedto equip 15-20 university students with the necessary competencies for formal sector employment. By the end of this training, participants will enhance thier job readiness and stand out as attractive candidates to potential employers.',
-        date: 'December 20, 2023',
+        
         images_url: [
             'https://staticlearn.shine.com/l/m/images/blog/career-growth-in-india.jpg',
         ]
     },
     {
+        title: 'Hi-Goal Camp 2022',
+        topic: 'ការចាប់ផ្ដើមអាជីវកម្ម និង​ សហក្រិនភាពវិទ្យាល័យ',
+        date: 'October 29-30, 2022',
+        images_url: [
+            'https://scontent.fpnh18-4.fna.fbcdn.net/v/t39.30808-6/313887126_494663456033495_1028697998296629990_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGAtiggQZXkomswOfFtdgN4ahXsc1izFwZqFexzWLMXBuvz-PSZk82cTX552pPET1Qyb9Os_HlInSmobtNkdgtR&_nc_ohc=TmynZrIN5S8AX_s4KNJ&_nc_ht=scontent.fpnh18-4.fna&oh=00_AfDjXnDM1_6mGex_Gn7UD_S94BTdPIYzg0hTl6ynlOiyfg&oe=64C8982A',
+        ]
+    },
+];
+{
+        title: 'Hi-Goal Camp 2021',
+        topic: 'សិក្ខាសាលាស្ដីពីថ្នាក់វិទ្យាសាស្រ្តពិត និង​ វិទ្យាសាស្ត្រសង្គម និង​ដំណើរឆ្ពោះទៅសាកលវិទ្យាល័យ',
+        date: 'December 19, 2021',
+        images_url: [
+            'https://images.pexels.com/photos/16401186/pexels-photo-16401186.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+            'https://images.pexels.com/photos/16401186/pexels-photo-16401186.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+        ]
+    },
+];
+{
+        title: 'Hi-Goal Camp 2020',
+        topic: 'ការត្រៀមខ្លួនសម្រាប់បន្តការសិក្សានៅមហាវិទ្យាល័យ',
+        date: 'December 26-27, 2020',
+        images_url: [
+            'https://images.pexels.com/photos/16401186/pexels-photo-16401186.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+            
+        ]
+    },
+];
+{
+        title: 'Hi-Goal Camp 2019',
+        topic: 'Know yourself',
+        date: 'December 22, 2019',
+        images_url: [
+            'https://images.pexels.com/photos/16401186/pexels-photo-16401186.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+            'https://images.pexels.com/photos/16401186/pexels-photo-16401186.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+        ]
+    },
+];
+{
         title: 'Event 2',
         topic: 'Topic in Event 2',
         date: 'December 20, 2024',
@@ -44,3 +85,4 @@ export const events = [
         ]
     },
 ];
+
