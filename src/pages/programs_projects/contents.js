a13028d7f2e30a24c5f5f03142b0623b6c627609
Sokmontrey <sythatsokmontrey@gmail.com>
2023-08-02 18:38:59 -0400

    fix some json mistake

--- src/pages/programs_projects/contents.js
+++ src/pages/programs_projects/contents.js
@@ -16,8 +16,8 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 export const programs = [
     {
         title: 'Hi-Goal Camp',
-        description: 'This program aims to prepare students for the rigors of higher education by providing them with the academic skills, knowledge, and mindset they need to succeed. The program will also help students develop their critical thinking, problem-solving, and communication skills.
-And then this program also aims to help beneficiaries develop the entrepreneurial mindset and skills they need to succeed in the Cambodia startup ecosystem and digital economy. The program will cover topics such as business planning, marketing, fundraising, and product development.'
+        description: `This program aims to prepare students for the rigors of higher education by providing them with the academic skills, knowledge, and mindset they need to succeed. The program will also help students develop their critical thinking, problem-solving, and communication skills.
+And then this program also aims to help beneficiaries develop the entrepreneurial mindset and skills they need to succeed in the Cambodia startup ecosystem and digital economy. The program will cover topics such as business planning, marketing, fundraising, and product development.`
     
     },
 
@@ -44,7 +44,6 @@ export const events = [
             'https://scontent.fpnh18-4.fna.fbcdn.net/v/t39.30808-6/313887126_494663456033495_1028697998296629990_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGAtiggQZXkomswOfFtdgN4ahXsc1izFwZqFexzWLMXBuvz-PSZk82cTX552pPET1Qyb9Os_HlInSmobtNkdgtR&_nc_ohc=TmynZrIN5S8AX_s4KNJ&_nc_ht=scontent.fpnh18-4.fna&oh=00_AfDjXnDM1_6mGex_Gn7UD_S94BTdPIYzg0hTl6ynlOiyfg&oe=64C8982A',
         ]
     },
-];
 {
         title: 'Hi-Goal Camp 2021',
         topic: 'សិក្ខាសាលាស្ដីពីថ្នាក់វិទ្យាសាស្រ្តពិត និង​ វិទ្យាសាស្ត្រសង្គម និង​ដំណើរឆ្ពោះទៅសាកលវិទ្យាល័យ',
@@ -54,8 +53,7 @@ export const events = [
             'https://images.pexels.com/photos/16401186/pexels-photo-16401186.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
         ]
     },
-];
-{
+    {
         title: 'Hi-Goal Camp 2020',
         topic: 'ការត្រៀមខ្លួនសម្រាប់បន្តការសិក្សានៅមហាវិទ្យាល័យ',
         date: 'December 26-27, 2020',
@@ -64,8 +62,7 @@ export const events = [
             
         ]
     },
-];
-{
+    {
         title: 'Hi-Goal Camp 2019',
         topic: 'Know yourself',
         date: 'December 22, 2019',
@@ -74,8 +71,7 @@ export const events = [
             'https://images.pexels.com/photos/16401186/pexels-photo-16401186.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
         ]
     },
-];
-{
+    {
         title: 'Event 2',
         topic: 'Topic in Event 2',
         date: 'December 20, 2024',
