fe03e049efe5080456871fe11b8400f8b47833f8
Sodana <114868249+ssdn7@users.noreply.github.com>
2023-09-15 13:41:46 +0700

    Update contents.js

--- src/pages/programs_projects/contents.js
+++ src/pages/programs_projects/contents.js
@@ -31,13 +31,13 @@ export const events = [
         title: 'Hi-Goal Camp 2022',
         topic: 'ការចាប់ផ្ដើមអាជីវកម្ម និង​ សហក្រិនភាពវិទ្យាល័យ',
         date: 'October 29-30, 2022'
-        images_url: ['https://scontent.fpnh7-1.fna.fbcdn.net/v/t39.30808-6/315155752_503039591862548_2615170479166749949_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=49d041&_nc_eui2=AeFoxS0E4NgMor7E2MzB_Ejsz45LkMjsPyjPjkuQyOw_KCCLON1ouiU5PkEQ0xCKuCqYj5fMkR_N5sXCzAA9W3WD&_nc_ohc=tP-VC_5wuY0AX9IfNjh&_nc_oc=AQlVy-UlXWGYWR56FwqyDpZODUMvC2hVRaxjATskYhv9PpMjf6gWkRz_cqnNijvgOlg&_nc_ht=scontent.fpnh7-1.fna&oh=00_AfDr8aGxNyU3S5f-PtT_SuyCJsFArk9PQUrhWym1M01Rwg&oe=6505DE59',]
+        images_url: ['https://images.pexels.com/photos/16401186/pexels-photo-16401186.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',]
     },
     {
         title: 'Hi-Goal Camp 2021',
         topic: 'សិក្ខាសាលាស្ដីពីថ្នាក់វិទ្យាសាស្រ្តពិត និង​ វិទ្យាសាស្ត្រសង្គម និង​ដំណើរឆ្ពោះទៅសាកលវិទ្យាល័យ',
         date: 'December 19, 2021',
-        images_url: ['https://scontent.fpnh7-1.fna.fbcdn.net/v/t39.30808-6/267050687_2037725753056467_853177772107036770_n.png?_nc_cat=109&ccb=1-7&_nc_sid=49d041&_nc_eui2=AeGA-P14odB2zOBl3yxg3NEUkS5Nj5O3E0-RLk2Pk7cTTwAmWcVYrHgKrH9TbEM_53bCsTFdv-n7zVwfxDJinlpn&_nc_ohc=085vgAnpDAYAX8Frbov&_nc_ht=scontent.fpnh7-1.fna&oh=00_AfAn-Eh_B7s26WjJhjJPW-splYlheqeDJvsQzTlJZnCy7g&oe=6506C52A',]
+        images_url: ['https://images.pexels.com/photos/16401186/pexels-photo-16401186.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',]
     },
 {
         title: 'Hi-Goal Camp 2020',
