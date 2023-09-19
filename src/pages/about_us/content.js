193adf2e7f441b566c793c7c3db7e5e06e8b6a83
Sodana <114868249+ssdn7@users.noreply.github.com>
2023-09-19 14:08:05 +0700

    Update content.js

--- src/pages/about_us/content.js
+++ src/pages/about_us/content.js
@@ -10,10 +10,7 @@ Vision Title
 
 export const vision_detail = `
 
-Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-occaecat fugiat aliqua. Anim aute id magna aliqua ad ad non
-deserunt sunt. Qui irure qui lorem cupidatat commodo.
+We envision a world where all high school students are well-prepared for the next chapter of their lives. We believe that every student has the potential to succeed, and we are committed to providing them with the resources and support they need to achieve their goals.
 
 `;
 
@@ -21,47 +18,41 @@ export const missions = [
   {
     name: "Mission 1",
     description:
-      "Aut illo quae. Ut et harum ea animi natus. Culpa maiores et sed sint et magnam exercitationem quia. Ullam voluptas nihil vitae dicta molestiae et. Aliquid velit porro vero.",
+      "Providing them with opportunities to learn from experienced professionals and mentors.",
   },
   {
     name: "Mission 2",
     description:
-      "Mollitia delectus a omnis. Quae velit aliquid. Qui nulla maxime adipisci illo id molestiae. Cumque cum ut minus rerum architecto magnam consequatur. Quia quaerat minima.",
+      "Offering programs and events that help them develop their skills and knowledge.",
   },
   {
     name: "Mission 3",
     description:
-      "Aut repellendus et officiis dolor possimus. Deserunt velit quasi sunt fuga error labore quia ipsum. Commodi autem voluptatem nam. Quos voluptatem totam.",
+      "Creating a supportive and empowering environment where they can explore their interests and passions.",
   },
 ];
 
 export const value_detail = `
 
-Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-voluptatum cupiditate veritatis in accusamus quisquam.
+Our value is equity, excellence, and empowerment. They are all important for helping high school students prepare for their future. By providing all students with access to high-quality programs and resources, by offering them opportunities to learn from experienced professionals, and by empowering them to reach their full potential, you can make a real difference in their lives.
 
 `;
 
 export const values_list = [
   {
-    name: "Value 1",
+    name: "Equity",
     description:
-      "Aut illo quae. Ut et harum ea animi natus. Culpa maiores et sed sint et magnam exercitationem quia. Ullam voluptas nihil vitae dicta molestiae et. Aliquid velit porro vero.",
+      "We believe that equity is essential for student success. This means that we are committed to creating a community where all students feel safe, supported, and valued. We also believe that all students should have access to the resources and opportunities they need to succeed, regardless of their background or circumstances.",
   },
   {
-    name: "Value 2",
+    name: "Excellence",
     description:
-      "Mollitia delectus a omnis. Quae velit aliquid. Qui nulla maxime adipisci illo id molestiae. Cumque cum ut minus rerum architecto magnam consequatur. Quia quaerat minima.",
+      "We strive to provide our students with the best possible education. This means that we offer a rigorous and challenging academic curriculum that is aligned with state and national standards. We also provide students with access to a variety of extracurricular activities and programs that help them develop their interests and talents.",
   },
   {
-    name: "Value 3",
+    name: "Empowerment",
     description:
-      "Aut repellendus et officiis dolor possimus. Deserunt velit quasi sunt fuga error labore quia ipsum. Commodi autem voluptatem nam. Quos voluptatem totam.",
-  },
-  {
-    name: "Value 4",
-    description:
-      "Magnam provident veritatis odit. Vitae eligendi repellat non. Eum fugit impedit veritatis ducimus. Non qui aspernatur laudantium modi. Praesentium rerum error deserunt harum.",
+      "We believe that students are capable of great things. We empower them to reach their full potential by providing them with opportunities to take on leadership roles, make decisions, and solve problems. We also encourage students to explore their interests and passions, and we provide them with the support they need to achieve their goals.",
   },
 ];
 
